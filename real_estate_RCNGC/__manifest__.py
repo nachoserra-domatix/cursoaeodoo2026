374a2b715f8e96b42063a6bb5eaea566427fff20
Francisco <136316427+rcngc1908@://github.com>
2026-09-21 12:20:33 +0000

    [real_estate_RCNGC] FEAT: Estructura inical del modulo

---
+++ real_estate_RCNGC/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Real Estate RCNGC',
+    'version': '1.0.0',
+    'summary': 'Real Estate Management Module',
+    'description': 'Real Estate RCNGC',
+    'author': 'RCNGC',
+    'category': 'Real Estate',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/realestate_property_views.xml',
+        'views/realestate_menuitems.xml',
+    ],
+    'installable': True,
+    'application': True
+}
